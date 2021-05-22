e398f8dbde2af35cc4d4d3676f772afe17f507e4
John Aldrich Bernardo <4ldrich@protonmail.com>
2021-05-22 16:40:13 +0800

    fix: remove hot

--- packages/app/src/client/App.js
+++ packages/app/src/client/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { hot } from 'react-hot-loader/root';
 
 import Routes from './Routes';
 
-const App = () => {
-  return hot(
+const App = () => (
     <BrowserRouter>
       <Routes />
     </BrowserRouter>
   );
-};
 
 export default App;
